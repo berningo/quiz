e5d919f61fcea941dbc521cda0a49286105c8625
Oliver Berning <oliver.berning@gmail.com>
2024-05-01 20:06:04 +0200

    Randomize order of answers if they are not yet visited

--- src/routes/questions/[question]/+page.server.ts
+++ src/routes/questions/[question]/+page.server.ts
@@ -1,15 +1,35 @@
 import { quiz } from '$lib/server/data'
 import { error } from '@sveltejs/kit';
 
-export function load({ params }) {
-    const question = quiz[Number(params.question)];
+function shuffleAnswers(answers: Answer[]) {
+    let curidx = answers.length;
+    for (let i = answers.length - 1; i > 0; i--) {
+        let rndidx = Math.floor(Math.random() * i);
+        let [a, b] = [answers[rndidx], answers[i]];
+        [answers[i], answers[rndidx]] = [answers[rndidx], answers[i]];
+    }
+}
+
+export function load({ params, cookies }) {
+    const page = Number(params.question);
+    const question = quiz[page];
+    
     if (!question) throw error(404);
+    
+    let visitedPages: number[] = JSON.parse(cookies.get("visitedPages") || "[]");
+    if (!visitedPages.includes(page)) {
+        shuffleAnswers(question.answers);
+        visitedPages.push(page);
+        cookies.set("visitedPages", JSON.stringify(visitedPages), { path: "/" });
+    }
+
     return {
         quizLength: quiz.length,
         question: {
             text: question.question,
             score: question.score,
             answers: question.answers
-        }
+        },
+        visitedPages
     }
 }
--- src/routes/report/+server.ts
+++ src/routes/report/+server.ts
@@ -2,10 +2,12 @@ import { json, type Cookies } from "@sveltejs/kit";
 import fs from "fs";
 
 
-export async function POST({ request }) {
+export async function POST({ request, cookies }) {
     const report = await request.text();
+    cookies.set("visitedPages", JSON.stringify([]), { path: "/" });
     
-    console.log("--------> " + report);
+    console.log("\n\n================ REPORT ================\n");
+    console.log(report);
     fs.promises.writeFile("report.txt", report, {
         flag: "w"
     }).then(() => {
